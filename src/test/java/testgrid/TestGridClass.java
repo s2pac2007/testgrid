6c04c60319e1aa53ba673810279a7fba11ebdb73
Oleksii Stupak <alekseyst@hustaff.huji.local>
2017-11-09 14:49:00 +0200

    testngsecond.xml

--- src/test/java/testgrid/TestGridClass.java
+++ src/test/java/testgrid/TestGridClass.java
@@ -8,6 +8,7 @@ import java.lang.InterruptedException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
@@ -17,16 +18,18 @@ import org.testng.annotations.Test;
 import java.util.Map;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.Proxy.ProxyType;
 
 public class TestGridClass {
 	public WebDriver driver;
 	public String URL, Node;
 	protected ThreadLocal<RemoteWebDriver> threadDriver = null;
 //	String browser = "firefox";
-	@Parameters("browser")
+	@Parameters({"browser","pr"})
+		
 	@BeforeTest
-	public void launchbrowser(String browser) throws MalformedURLException {
+	public void launchbrowser(String browser,String pr) throws MalformedURLException {
 		//String URL = "http://www.calculator.net";
 
 		if (browser.equalsIgnoreCase("firefox")) {
@@ -35,75 +38,62 @@ public class TestGridClass {
 			DesiredCapabilities cap = DesiredCapabilities.firefox();
 			cap.setBrowserName("firefox");
 			driver = new RemoteWebDriver(new URL(Node), cap);
-			// Puts an Implicit wait, Will wait for 10 seconds before throwing
-			// exception
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			// Launch website
-			//driver.navigate().to(URL);
-			//driver.manage().window().maximize();
 		} else if (browser.equalsIgnoreCase("chrome")) {
 			System.out.println(" Executing on CHROME");
-		//ChromeDriverManager.getInstance().setup();
-		//ChromeOptions options = new ChromeOptions();
-		//options.addArguments("--headless", "--disable-gpu");
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		cap.setBrowserName("chrome");
-		//cap.setVersion("62");
-
-			//ChromeDriverManager.getInstance().setup();
-
-			//options.put("binary", "/usr/lib/chromium-browser/chromium-browser");
-			//options.put("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
-
-			//cap.setVersion("62");
-			//cap.setPlatform("Ubuntu"); 
-			//cap.setCapability("platform", "Ubuntu");
-			//cap.setCapability("", "--no-sandbox");
-			//cap.setCapabilities("chrome.switches", Arrays.asList("--start-maximized"));
+			DesiredCapabilities cap = DesiredCapabilities.chrome();
+			cap.setBrowserName("chrome");
 			String Node = "http://192.168.137.129:4444/wd/hub";
-			System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
 			driver = new RemoteWebDriver(new URL(Node), cap);
-			//driver = new RemoteWebDriver(new URL(Node), options.ToCapabilities());
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-			// Launch website
-			//driver.navigate().to(URL);
-			//driver.manage().window().maximize();
 		}  else {
 			throw new IllegalArgumentException("The Browser Type is Undefined");
 		}
+		
+		if (pr.equalsIgnoreCase("yes")) {
+		//Proxy
+			String url = "42.117.1.78:3128";
+			Proxy proxy = new Proxy();
+			proxy.setProxyType(ProxyType.MANUAL);
+			proxy.setHttpProxy(url);
+			DesiredCapabilities cap = DesiredCapabilities.firefox();
+			cap.setCapability(CapabilityType.PROXY,proxy);
+			driver = new RemoteWebDriver(new URL(Node), cap);
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		} else if (browser.equalsIgnoreCase("no")) {
+			String Node = "http://192.168.137.129:4444/wd/hub";
+			DesiredCapabilities cap = DesiredCapabilities.firefox();
+			cap.setBrowserName("firefox");
+			driver = new RemoteWebDriver(new URL(Node), cap);
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}  else {
+			throw new IllegalArgumentException("The Browser Type is Undefined");
+		}
+		
 	}
 
 	@Test
 	public void calculatepercent() throws InterruptedException {
-		//driver.navigate().to("https://www.google.com");
+		
 		driver.get("https://www.youtube.com/watch?v=KPQhbSKx0E8");
 		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(300000);
+		Thread.sleep(150000);
 		
 		driver.get("https://youtu.be/e0_CiFfa5pM");
   		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(300000);
+		Thread.sleep(150000);
 		
 		driver.get("https://www.youtube.com/watch?v=2BBw6xX5858");
   		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(300000);
+		Thread.sleep(150000);
 		
 		driver.get("https://www.youtube.com/watch?v=yKeXgQ5yDAM");
   		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(300000);
+		Thread.sleep(150000);
 		
 		driver.get("https://www.youtube.com/watch?v=e0_CiFfa5pM");
   		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(300000);
-		
-	//System.out.println("Launched page https://www.google.com");
-	// boolean title = driver.getTitle() == "Google class";
-	//System.out.println(driver.getTitle()+" class");
-    //if(title)
-    //  System.out.println("I am working correctly");
-    //else if(!title)
-	//  System.out.println("I am braiken!");
+		Thread.sleep(150000);
 
 		}
 	@AfterTest
