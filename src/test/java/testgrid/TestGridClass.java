200074013401e6b52e10df9b1d61dda96f415ab1
Oleksii Stupak <alekseyst@hustaff.huji.local>
2017-11-12 08:49:00 +0200

    Only one Url

--- src/test/java/testgrid/TestGridClass.java
+++ src/test/java/testgrid/TestGridClass.java
@@ -64,23 +64,23 @@ public class TestGridClass {
 		
 		driver.get("https://www.youtube.com/watch?v=KPQhbSKx0E8");
 		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(150000);
+		Thread.sleep(60000);
 		
-		driver.get("https://youtu.be/e0_CiFfa5pM");
-  		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(150000);
+		//driver.get("https://youtu.be/e0_CiFfa5pM");
+  		//driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+		//Thread.sleep(60000);
 		
-		driver.get("https://www.youtube.com/watch?v=2BBw6xX5858");
-  		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(150000);
+		//driver.get("https://www.youtube.com/watch?v=2BBw6xX5858");
+  		//driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+		//Thread.sleep(60000);
 		
-		driver.get("https://www.youtube.com/watch?v=yKeXgQ5yDAM");
-  		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(150000);
+		//driver.get("https://www.youtube.com/watch?v=yKeXgQ5yDAM");
+  		//driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+		//Thread.sleep(60000);
 		
-		driver.get("https://www.youtube.com/watch?v=e0_CiFfa5pM");
-  		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		Thread.sleep(150000);
+		//driver.get("https://www.youtube.com/watch?v=e0_CiFfa5pM");
+  		//driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+		//Thread.sleep(60000);
 
 		}
 	@AfterTest
